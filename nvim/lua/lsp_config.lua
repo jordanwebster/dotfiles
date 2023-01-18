00cf302ab8890bad85b8120aea7f9e520501f321
Jordan Webster <5429117+jordanwebster@users.noreply.github.com>
2023-01-18 13:40:29 +0000

    Add Go LSP server

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -32,7 +32,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local servers = {'rust_analyzer'}
+local servers = {'rust_analyzer', 'gopls'}
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
